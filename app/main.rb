0d036ecc729448619310a97c4660e9ecaeba0c8f
Jeremiah Donley <108106416+WeiQiPro@users.noreply.github.com>
2024-05-04 00:08:11 -0400

    2d logic, 3d render
    separated concerns, still rocky.

--- app/main.rb
+++ app/main.rb
@@ -1,75 +1,94 @@
-$width = 1280
-$height = 720
+# Constants for the window size
+$WIDTH = 1280
+$HEIGHT = 720
 
-def controls(inputs)
-  move_amount = 5
-  offset_angle = 90
+# Camera class to hold camera properties
+class Camera
+  attr_accessor :x, :y, :z, :fov, :zfar, :znear, :angle, :y_offset, :x_offset
 
-  $CAM.y_offset += 5 if inputs.key_held.w
-  $CAM.y_offset -= 5 if inputs.key_held.s
-
-  $CAM.x_offset += 5 if inputs.key_held.d
-  $CAM.x_offset -= 5 if inputs.key_held.a
-
-  $CAM.angle -= 5 if inputs.key_held.right_arrow
-  $CAM.angle += 5 if inputs.key_held.left_arrow
-
-  $CAM.angle %= 360
+  def initialize(x:, y:, z:, fov:, zfar:, znear:, angle:, y_offset:, x_offset:)
+    @x = x
+    @y = y
+    @z = z
+    @fov = fov
+    @zfar = zfar
+    @znear = znear
+    @angle = angle
+    @y_offset = y_offset
+    @x_offset = x_offset
+  end
 end
 
-def world_to_screen x, y, z, a
+class Sprite
+  attr_accessor :x, :y, :z, :a, :stack, :w, :h, :source
+
+  def initialize(x:, y:, z:, a:, stack:, w:, h:, source:)
+    @x = x
+    @y = y
+    @z = z
+    @a = a
+    @stack = stack
+    @w = w
+    @h = h
+    @source = source
+  end
+end
+
+# Function to handle user controls
+def controls(inputs, camera)
+  move_amount = 5
+
+  camera.y += move_amount if inputs.key_held.w
+  camera.y -= move_amount if inputs.key_held.s
+
+  camera.x += move_amount if inputs.key_held.d
+  camera.x -= move_amount if inputs.key_held.a
+
+  camera.angle -= inputs.key_held.right_arrow ? 5 : 0
+  camera.angle += inputs.key_held.left_arrow ? 5 : 0
+
+  camera.angle %= 360
+
+end
+
+# Function to convert 3D world coordinates to screen coordinates
+def world_to_screen(x, y, z, camera)
   # Calculate the distance from the camera
-  distance = z - $CAM.z
+  distance = z - camera.z
 
   # Calculate the scale based on the distance and field of view
-  scale = $CAM.zfar / ($CAM.zfar - $CAM.znear) * $CAM.fov / (distance + $CAM.znear)
+  scale = camera.zfar / (camera.zfar - camera.znear) * camera.fov / (distance + camera.znear)
 
-  # Calculate the screen x-coordinate based on the angle and distance
-  rel_x = x - $CAM.x + $CAM.x_offset
-  rel_y = y - $CAM.y + $CAM.y_offset
-  screen_x = rel_x * Math.cos($CAM.angle * Math::PI / 180) - rel_y * Math.sin($CAM.angle * Math::PI / 180) + $width / 2
+  # Calculate the screen x-coordinate based on the angle and distance, keeping the camera at the center of the screen
+  rel_x = x + camera.x_offset + camera.x
+  rel_y = y + camera.y_offset + camera.y
+  screen_x = rel_x * Math.cos(camera.angle * Math::PI / 180) - rel_y * Math.sin(camera.angle * Math::PI / 180) + $WIDTH / 2
 
-  # Calculate the screen y-coordinate based on the angle and distance
-  screen_y = rel_x * Math.sin($CAM.angle * Math::PI / 180) + rel_y * Math.cos($CAM.angle * Math::PI / 180) + $height / 2
+  # Calculate the screen y-coordinate based on the angle and distance, keeping the camera at the center of the screen
+  screen_y = rel_x * Math.sin(camera.angle * Math::PI / 180) + rel_y * Math.cos(camera.angle * Math::PI / 180) + $HEIGHT / 2
 
-  # Calculate the angle difference between the camera's angle and the sprite's initial angle
-  angle_diff = $CAM.angle - a
-
-  # Normalize the angle difference to be between -180 and 180 degrees
-  angle_diff = (angle_diff + 180) % 360 - 180
-
-  # Calculate the sprite's new angle based on the camera's angle and the angle difference
-  angle = a + angle_diff
-  # Return the screen coordinates and scale
-  return {
-    x: screen_x + $CAM.x_offset,
-    y: screen_y + $CAM.y_offset,
-    scale: scale,
-    angle: angle
-  }
+  { x: screen_x, y: screen_y, scale: scale }
 end
 
-def draw_sprite_stack sprite
+
+# Function to render a sprite stack
+def render_sprite_stack(sprite, camera)
   stack = []
-  wts = world_to_screen(sprite.x, sprite.y, sprite.z, sprite.a)
+  wts = world_to_screen(sprite.x, sprite.y, sprite.z, camera)
 
-  #don't draw if scale is less than 0
-  if wts && wts[:scale] > 0 
-
-    #stack sprites vertically by index * 2
-    #needs to be adjusted and offset by individual sprites
+  if wts && wts[:scale] > 0
     sprite.stack.times do |index|
       stack << {
         x: wts[:x],
         y: wts[:y] + index * 2,
-        w: sprite[:w] * wts[:scale],
-        h: sprite[:h] * wts[:scale],
+        w: sprite.w * wts[:scale],
+        h: sprite.h * wts[:scale],
         path: sprite.source,
         source_x: 0,
-        source_y: sprite[:h] * index,
-        source_w: sprite[:w],
-        source_h: sprite[:h],
-        angle: wts.angle #should always face the direction it started
+        source_y: sprite.h * index,
+        source_w: sprite.w,
+        source_h: sprite.h,
+        angle: wts.angle,
       }
     end
   end
@@ -77,111 +96,74 @@ def draw_sprite_stack sprite
   stack
 end
 
-def create_sprite(x, y, z, angle, stack, slice_w, slice_h, path, render_target)
-  sprite = {
-              x: x,
-              y: y,
-              z: z,
-              a: angle, #starting angle or facing direction
-              stack: stack, #total slices
-              direction: :vertical, # based on how the sprite sheet is made vertical or horizontals / not implemented
-              w: slice_w, #slice width
-              h: slice_h, #slice height
-              source: path, # sprite path
-              rt: render_target #for render_targets
-  }
 
-  sprite
+def WorldUpdate(objects, camera, inputs)
+  objects.each do |object|
+    # Translate object to camera position
+    object.x -= camera.x_offset + camera.x
+    object.y -= camera.y_offset + camera.y
+
+    # Rotate object around the player character's position
+    if inputs.key_down.right_arrow
+      object.x, object.y = rotate_point(object.x, object.y, 0, 0, -5)
+    elsif inputs.key_down.left_arrow
+      object.x, object.y = rotate_point(object.x, object.y, 0, 0, 5)
+    end
+
+    # Translate object back to original position
+    object.x += camera.x_offset + camera.x
+    object.y += camera.y_offset + camera.y
+  end
 end
 
-# def draw_sprite_target sprite
-#   wts = world_to_screen(sprite.x, sprite.y, sprite.z, sprite.a)
-  
-#   #don't draw if scale is less than 0
-#   if wts && wts[:scale] > 0 
-#     return {
-#         x: wts[:x],
-#         y: wts[:y],
-#         w: sprite[:w] * wts[:scale],
-#         h: sprite[:h] * wts[:scale],
-#         path: sprite.rt,
-#         angle: wts.angle #should always face the direction it started
-#     }
-#   end
-# end
-
-# def create_sprite_target(x, y, z, angle, stack, slice_w, slice_h, path, render_target, args)
-#   sprite = {
-#     x: x,
-#     y: y,
-#     z: z,
-#     a: angle, #starting angle or facing direction
-#     stack: stack, #total slices
-#     direction: :vertical, # based on how the sprite sheet is made vertical or horizontals / not implemented
-#     w: slice_w, #slice width
-#     h: slice_h, #slice height
-#     source: path, # sprite path
-#     rt: render_target #for render_targets
-#   }
 
 
-#   wts = world_to_screen(x, y, z, angle)
-
-#   rt = args.outputs[render_target]
-#   rt.w = slice_w * wts.scale
-#   rt.h = slice_h * wts.scale
-
-#   stack.times do |index|
-#     rt.sprites << {
-#       x: 0,
-#       y: 0 + (index * 2),
-#       w: slice_w * wts.scale,
-#       h: slice_h * wts.scale,
-#       path: "sprites/house_stack.png",
-#       source_x: 0,
-#       source_y: slice_h * index,
-#       source_w: slice_w,
-#       source_h: slice_h,
-#       angle: angle
-#     }
-#   end
-
-#   rt.w = slice_w * wts.scale
-#   rt.h = slice_h * wts.scale
-
-#   puts rt.sprites
-#   puts sprite
-
-#   sprite
-# end
-
-$CAM = {
-  x: 0,
-  y: 0,
-  z: 60,
-  fov: 90,
-  zfar: 100,
-  znear: 10,
-  zfarnear: 1 /(100 - 10),
-  pitch: -30, #top down view
-  angle: 0,
-  y_offset: 0,
-  x_offset: 0
-}
+def rotate_point(x, y, cx, cy, angle)
+  radians = angle * Math::PI / 180
+  new_x = Math.cos(radians) * (x - cx) - Math.sin(radians) * (y - cy) + cx
+  new_y = Math.sin(radians) * (x - cx) + Math.cos(radians) * (y - cy) + cy
+  [new_x, new_y]
+end
 
 
+def populate_world(num_objects)
+  objects = []
 
-def tick args
+  num_objects.times do
+    # Generate random coordinates within a circle of radius 2000 around the origin
+    radius = rand(2000)
+    angle = rand(360)
+    x = radius * Math.cos(angle * Math::PI / 180)
+    y = radius * Math.sin(angle * Math::PI / 180)
 
-  if args.tick_count == 0
-    args.state.house ||= create_sprite(0, 0, 100, 45, 49, 96, 58, "sprites/house_stack.png", :house_stack)
-    # args.state.house2 ||= create_sprite_target(200, 200, 100, 90, 49, 96, 58, "sprites/house_stack.png", :house_stack_two, args)
-    puts args.state.house2
+    # Create a house-like object
+    object = Sprite.new(x: x, y: y, z: 100, a: 45, stack: 49, w: 96, h: 58, source: "sprites/house_stack.png")
+    objects << object
   end
 
-  controls(args.inputs.keyboard);
-
-  args.outputs.sprites << draw_sprite_stack(args.state.house)
-  # args.outputs.sprites << draw_sprite_target(args.state.house2)
-  args.outputs.debug << "CAM Angle: #{$CAM.angle}"
+  objects
 end
+
+# Create initial camera
+$CAMERA = Camera.new(x: 0, y: 0, z: 50, fov: 90, zfar: 1000, znear: 10, angle: 0, y_offset: $HEIGHT / 2, x_offset: $WIDTH / 2)
+
+
+def tick(args)
+  if args.tick_count == 0
+    args.state.objects = populate_world(100) # Populate the world with 100 objects
+  end
+
+  controls(args.inputs.keyboard, $CAMERA)
+
+  # Update the world with the populated objects
+  WorldUpdate(args.state.objects, $CAMERA, args.inputs.keyboard)
+
+  args.state.objects.each do |sprite|
+    args.outputs.sprites << render_sprite_stack(sprite, $CAMERA)
+  end
+
+  args.outputs.debug << "CAM X: #{$CAMERA.x}"
+  args.outputs.debug << "CAM Y: #{$CAMERA.y}"
+  args.outputs.debug << "CAM Angle: #{$CAMERA.angle}"
+end
+
