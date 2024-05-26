0f9ba3f2b6a7543f88da349d46e95254f748d36f
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-05-26 11:59:16 +0900

    Properly implement camera transformation

--- app/kf_okarin/camera.rb
+++ app/kf_okarin/camera.rb
@@ -11,7 +11,18 @@ module KfOkarin
     end
 
     def transform_object(object)
-      { x: 640, y: 360, perspective: @perspective }
+      pitch_sin = Math.sin(@perspective.pitch.to_radians)
+      relative_x = object[:x] - @center_x
+      relative_y = object[:y] - @center_y
+      yaw_sin = Math.sin(@perspective.yaw.to_radians)
+      yaw_cos = Math.cos(@perspective.yaw.to_radians)
+      rotated_x = relative_x * yaw_cos - relative_y * yaw_sin
+      rotated_y = relative_x * yaw_sin + relative_y * yaw_cos
+      {
+        x: 640 + rotated_x * @perspective.scale,
+        y: 360 + (rotated_y * @perspective.scale) * pitch_sin,
+        perspective: @perspective
+      }
     end
   end
 end
